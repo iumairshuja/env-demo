aa38d805733f1d3ed5d7437cac7b1d50f0086a0e
Umair Shuja <mohammadumair142@gmail.com>
2025-01-21 17:18:27 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // Here you can add code to handle the form submission, e.g., sending data to a server.
+});
